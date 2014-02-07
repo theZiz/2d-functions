4a96de0e969e61013fa08fc9870615c719c45b58
Babs Seed <babs@Manehattan>
2014-02-07 13:37:10 +0100

    Changes for the change of the button design

--- ebba.c
+++ ebba.c
@@ -198,14 +198,14 @@ void resize( Uint16 w, Uint16 h )
 	spFontSetShadeColor(0);
 	font = spFontLoad( "./data/DejaVuSans-Bold.ttf", 6 * spGetSizeFactor() >> SP_ACCURACY );
 	spFontAdd( font, SP_FONT_GROUP_ASCII"°αβγεφσ√ᵥ₁₂₃", 65535 ); //whole ASCII
-	spFontAddButton( font, 'R', SP_BUTTON_START_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); //Return == START
-	spFontAddButton( font, 'B', SP_BUTTON_SELECT_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); //Backspace == SELECT
-	spFontAddButton( font, 'q', SP_BUTTON_L_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); // q == L
-	spFontAddButton( font, 'e', SP_BUTTON_R_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); // e == R
-	spFontAddButton( font, 'a', SP_BUTTON_LEFT_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); //a == left button
-	spFontAddButton( font, 'd', SP_BUTTON_RIGHT_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); // d == right button
-	spFontAddButton( font, 'w', SP_BUTTON_UP_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); // w == up button
-	spFontAddButton( font, 's', SP_BUTTON_DOWN_NOWASD_NAME, 65535, SP_ALPHA_COLOR ); // s == down button
+	spFontAddButton( font, 'R', SP_BUTTON_START_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); //Return == START
+	spFontAddButton( font, 'B', SP_BUTTON_SELECT_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); //Backspace == SELECT
+	spFontAddButton( font, 'q', SP_BUTTON_L_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); // q == L
+	spFontAddButton( font, 'e', SP_BUTTON_R_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); // e == R
+	spFontAddButton( font, 'a', SP_BUTTON_LEFT_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); //a == left button
+	spFontAddButton( font, 'd', SP_BUTTON_RIGHT_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); // d == right button
+	spFontAddButton( font, 'w', SP_BUTTON_UP_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); // w == up button
+	spFontAddButton( font, 's', SP_BUTTON_DOWN_NOWASD_NAME, 65535, spGetRGB(127,127,127) ); // s == down button
 	spFontMulWidth(font,spFloatToFixed(0.85f));
 	spFontAddBorder(font , 0);//spGetRGB(128,128,128));
 	spSelectRenderTarget(screen);
